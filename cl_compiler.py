f3e1a3e90adbd988ddc658b809a8bbca34a552ec
Peter Schmidt-Nielsen <snp@mit.edu>
2017-03-05 04:31:16 -0500

    I'm going to initially implement the bytecode compiler in Python, then later convert into C++.

---
+++ cl_compiler.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# Python based Cl byte code compiler.
+# This is a reference-only implementation in Python, before I launch into reimplementing this in C++.
+
+import parse
+import assemble
+
