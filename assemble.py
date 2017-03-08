4acd6bf3c0ba262857cc1a4663ff77ab6b2dd158
Peter Schmidt-Nielsen <snp@mit.edu>
2017-03-08 04:58:02 -0500

    Updating the assembler to have labels and jumps.

--- assemble.py
+++ assemble.py
@@ -30,19 +30,37 @@ for i, op in enumerate(opcodes):
 
 mapping = {op["name"]: op for op in opcodes}
 
-def make_ast(s):
-	output = []
+class AssemblyUnit:
+	"""AssemblyUnit
+
+	Corresponds to a scope in which instruction counters are contiuous.
+	Thus, labels and jumps only make sense at the level of an AU.
+	Each function has its own AU, as well as the global level.
+	"""
+	def __init__(self):
+		self.labels = {}
+		self.ast = []
+		self.instruction_counter = 0
+
+def make_assembly_unit(s):
 	s = s.replace(";", "\n")
-	ast = []
-	stack = [ast]
+	assembly_unit = AssemblyUnit()
+	stack = [assembly_unit]
 
 	for line in s.split("\n"):
 		line = line.split("#")[0].strip()
 		if not line:
 			continue
 
+		# If the line ends with a colon, then it's a label.
+		if line.endswith(":"):
+			label_name = line[:-1]
+			stack[-1].labels[label_name] = stack[-1].instruction_counter
+			continue
+
 		# We now pull out the first token to get the opcode name.
 		opcode_name = line.split()[0]
+		stack[-1].instruction_counter += 1
 
 		# If we hit a "}" then we pop out one level of function-definition.
 		if opcode_name == "}":
@@ -71,8 +89,7 @@ def make_ast(s):
 				else:
 					data_field = data_field.decode("hex")
 
-			args = map(int, args)
-			stack[-1].append({"name": opcode_name, "args": args, "data_field": data_field})
+			stack[-1].ast.append({"name": opcode_name, "args": args, "data_field": data_field})
 		else:
 			# First off, in the case of a MAKE_FUNCTION, demand that the args end with "{", then strip it.
 			assert len(args) == 1
@@ -84,30 +101,35 @@ def make_ast(s):
 			for i in args[1:]:
 				load_index, store_index = map(int, i.split("->"))
 				subscope_closure_descriptors.append((load_index, store_index))
-			new_ast_scope = []
-			stack[-1].append({
+			new_assembly_unit = AssemblyUnit()
+			stack[-1].ast.append({
 				"name": "MAKE_FUNCTION",
 				"args": [],
 				"subscope_length": subscope_length,
 				"subscope_closure_descriptors": subscope_closure_descriptors,
-				"contents": new_ast_scope,
+				"contents": new_assembly_unit,
 			})
-			stack.append(new_ast_scope)
+			stack.append(new_assembly_unit)
 
 	assert len(stack) == 1, "Missing } to close some MAKE_FUNCTION!"
 
-	return ast
+	return assembly_unit
 
-def assemble(ast):
+def assemble(assembly_unit):
 	output = []
-	for op in ast:
+	for op in assembly_unit.ast:
 		# Encode the 1-byte opcode.
 		opcode_description = mapping[op["name"]]
 		output.append(chr(opcode_description["index"]))
 
+		# If we're a jump, do special handling to compute our argument.
+		is_jump = op["name"] in ["JUMP", "JUMP_IF_TRUTHY", "JUMP_IF_FALSEY"]
+		if is_jump:
+			op["args"] = map(assembly_unit.labels.__getitem__, op["args"])
+
 		# Encode the arguments.
 		for arg in op["args"]:
-			output.append(struct.pack("<q", arg))
+			output.append(struct.pack("<q", int(arg)))
 
 		data_field = None
 
@@ -133,11 +155,21 @@ def assemble(ast):
 
 	return "".join(output)
 
+	# These three values are used for computing jump targets.
+	opcode_index = 0
+	labels = {}
+	forwards_references = []
+
 	s = s.replace(";", "\n")
 	for line in s.split("\n"):
 		line = line.split("#")[0].strip()
 		if not line:
 			continue
+
+		# If the line ends with a colon, then it's a label.
+		if line.endswith(":"):
+			label_name = line[:-1]
+			labels[label_name] = opcode_index
 
 		# We now pull out the first token to get the opcode name.
 		opcode_name = line.split()[0]
@@ -157,23 +189,42 @@ def assemble(ast):
 			assert data_field.startswith('"') and data_field.endswith('"')
 			data_field = data_field[1:-1]
 
+		# If we're a jump, do special handling to compute our argument.
+		is_jump = opcode_name in ["JUMP", "JUMP_IF_TRUTHY", "JUMP_IF_FALSEY"]
+
 		# Do the actual encoding.
+		opcode_index += 1
 		output.append(chr(op["index"]))
-		for arg in args:
-			output.append(struct.pack("<q", int(arg)))
+		# Now we encode the arguments.
+		if is_jump:
+			# Jumps get special encoding -- we insert a None, and save the index into output so we can patch it up later.
+			forwards_references.append((len(output), args[0]))
+			output.append(None)
+		else:
+			# Non-jumps simply encode their arguments as integers.
+			for arg in args:
+				output.append(struct.pack("<q", int(arg)))
 		if op["data_field"]:
 			output.append(struct.pack("<I", len(data_field)))
 			output.append(data_field)
+
+	print "Jump table:", labels
+
+	# Patch up the forwards references.
+	for output_index, label_name in forwards_references:
+		assert label_name in labels, "Undefined label: %r" % (label_name,)
+		jump_target = labels[label_name]
+		output[output_index] = struct.pack("<q", jump_target)
 
 	return "".join(output)
 
 if __name__ == "__main__":
 	with open("source.cla") as f:
-		input_source = f.read()	
-	ast = make_ast(input_source)
-	__import__("pprint").pprint(ast)
-	bytecode = assemble(ast)
-	print bytecode.encode("hex")
+		_input_source = f.read()	
+	_assembly_unit = make_assembly_unit(_input_source)
+	__import__("pprint").pprint(_assembly_unit)
+	_bytecode = assemble(_assembly_unit)
+	print _bytecode.encode("hex")
 	with open("bytecode.cl", "w") as f:
-		f.write(bytecode)
+		f.write(_bytecode)
 
